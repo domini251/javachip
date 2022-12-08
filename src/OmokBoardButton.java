fa470f569a9edf88dda00ab96289202587c15705
domini251 <kdm030901@gmail.com>
2022-12-08 16:22:22 +0900

    Omok, Button, frame has been modified

---
+++ src/OmokBoardButton.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class OmokBoardButton extends JButton implements OmokBoardButtonInterface{
+    private ImageIcon none = new ImageIcon();
+    private ImageIcon black = new ImageIcon();
+    private ImageIcon white = new ImageIcon();
+    private OmokRegisterInterface r;
+    private OmokFrameInterface f;
+    private int x;
+    private int y;
+    private int onRock = 0;
+
+    /**
+     * 오목칸의 상태를 나타냄. 눌렸을 때 플레이어에 따라 맞는 색 돌을 정해줌.
+     * @param frame 프래임
+     * @param x_pos 이 버튼의 판 x좌표
+     * @param y_pos 이 버튼의 판 y좌표
+     */
+    public OmokBoardButton(OmokFrameInterface frame, int x_pos, int y_pos) {
+        f = frame;
+        x = x_pos;
+        y = y_pos;
+        Image N = none.getImage();
+        N = N.getScaledInstance(60,60,Image.SCALE_SMOOTH);
+        none = new ImageIcon(N);
+        Image B = black.getImage();
+        B = B.getScaledInstance(60,60,Image.SCALE_SMOOTH);
+        black = new ImageIcon(B);
+        Image W = white.getImage();
+        W = W.getScaledInstance(60,60,Image.SCALE_SMOOTH);
+        white = new ImageIcon(W);
+        setIcon(none);
+        if (f.userNumber() == 1)
+            setDisabledSelectedIcon(black);
+        else if (f.userNumber() == 2)
+            setDisabledSelectedIcon(white);
+        setPreferredSize(new Dimension(60,60));
+    }
+    /**
+     * 이 위치에 어떤 돌이 있는지 확인
+     * @return 있다면 참 없다면 거짓
+     */
+    public boolean exist() {
+        if (onRock == 0)
+            return false;
+        else {
+            onRock = 1;
+            return true;
+        }
+    }
+    /**
+     * 버튼의 상태를 바꿔줌
+     * @return
+     */
+    public void buttonReset() {
+        onRock = 0;
+        setEnabled(true);
+    }
+    /**
+     * 버튼이 눌렸을때 이미지가 바뀜
+     * @param e
+     */
+    public void actionPerformed(ActionEvent e) {
+        setEnabled(false);
+
+    }
+}
--- src/OmokFrameInterface.java
+++ src/OmokFrameInterface.java
@@ -1,3 +1,22 @@
+/**
+ * int[][] plate를 가지고 BoardButton[][] OmokButtonArray와 판정해서 프레임을 화면에 보여줌
+ */
 public interface OmokFrameInterface {
+	/**
+	 * 이 위치에 돌을 놓음
+	 * @param row 가로
+	 * @param col 세로
+	 */
+	public void setStone(int row, int col);
+	/**
+	 * plate, OmokButtonArray 초기화 시키기
+	 */
+	public void reset();
 
+	/**
+	 * 현재 차례인 플레이어의 번호
+	 * @return 1은 흑돌 2는 백돌
+	 */
+	public int userNumber();
+	
 }
